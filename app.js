40440ac84f2f595bd9cf57622235eca31dac1afd
dillin92 <dillin970@gmail.com>
2021-06-04 02:23:34 -0400

    gathered user input using inquirer

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-const fs = require('fs');
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
-const generatePage = require('./src/page-template.js')
+ const inquirer = require('inquirer');
+ console.log(inquirer);
+
+ inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+// const fs = require('fs');
+
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const [name, github] = profileDataArgs;
+// const generatePage = require('./src/page-template.js')
 
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//   if (err) throw new Error(err);
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
